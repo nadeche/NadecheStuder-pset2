0666cd12068e4d8ced8acf55ac212afe627ff89e
Nadeche <nadeche.studer@gmail.com>
2016-04-22 15:30:21 +0200

    Added next activity FillStory class and xml portrait. StoryChoice.java implementation of onListItemClickListener with short toast and open next activity (FillStory) with selected story name. Landscape version of story_choice completed. Modification to manifest to add FillStory activity

---
+++ app/src/main/java/com/example/nadeche/nadechestuder_pset2/FillStory.java
@@ -0,0 +1,15 @@
+package com.example.nadeche.nadechestuder_pset2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/*
+ * Created by Nadeche on 22-4-2016.
+ */
+public class FillStory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fill_story);
+    }
+}
--- app/src/main/java/com/example/nadeche/nadechestuder_pset2/StoryChoice.java
+++ app/src/main/java/com/example/nadeche/nadechestuder_pset2/StoryChoice.java
@@ -1,11 +1,16 @@
 package com.example.nadeche.nadechestuder_pset2;
 
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -35,6 +40,7 @@ public class StoryChoice extends AppCompatActivity {
             stories[i] = stories[i].replaceFirst(".txt", "");
         }
 
+        //randomize the order of the list
         Collections.shuffle(Arrays.asList(stories));
 
         //list adapter
@@ -45,5 +51,27 @@ public class StoryChoice extends AppCompatActivity {
 
         //fill the list view
         storyChoice.setAdapter(storyList);
+
+        //listen for list item clicks
+        storyChoice.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //get the selected story to a string
+                //String chosenStory = (String) parent.getItemAtPosition(position);
+                String chosenStory = ((TextView)view).getText().toString();
+
+                //display a short toast
+                Toast.makeText(StoryChoice.this,chosenStory, Toast.LENGTH_SHORT).show();
+
+                //create intent to go to next activity
+                Intent next = new Intent(getApplicationContext(),FillStory.class );
+
+                //bring chosen story to next activity
+                next.putExtra("story", chosenStory);
+                //launch next activity
+                startActivity(next);
+            }
+        });
     }
 }
